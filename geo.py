c4eacebc28dfec2bd6179a008064ab1fb22d216d
Johnathan Graydon <johnathan99j@yahoo.com>
2017-10-28 21:52:21 +0100

    Update geo.py to give coords for a given cutomer ID

--- geo.py
+++ geo.py
@@ -3,13 +3,24 @@ import json
 
 from envVariables import *
 
-r = requests.get("http://api.reimaginebanking.com/merchants?key=" + API_KEY)
+i, j = 0, 0
 
-i = 0
+# cust id -> account(s) id -> purchase(s) id -> merchant id -> gps
+custId = "59f45071a73e4942cdafe49f"
+
+r = requests.get("http://api.reimaginebanking.com/customers/" + custId + "/accounts?key=" + API_KEY)
 
 try:
-    while r.json()['data'][i]['geocode'] != "":
-        print("lat:" + str(r.json()['data'][i]['geocode']['lat']) + " lng:" + str(r.json()['data'][i]['geocode']['lng']))
+    # Loop though all the customers accounts
+    while r.json()[i]['_id'] != "":
+        s = requests.get("http://api.reimaginebanking.com/accounts/" + str(r.json()[i]['_id']) + "/purchases?key=" + API_KEY)
+
+        # Loop though all the purchases in this account
+        while s.json()[j]['merchant_id'] != "":
+            t = requests.get("http://api.reimaginebanking.com/merchants/" + s.json()[j]['merchant_id'] + "?key=" + API_KEY)
+
+            print("lat:" + str(t.json()['geocode']['lat']) + " lng:" + str(t.json()['geocode']['lng']))
+            j += 1
         i += 1
 except IndexError as e:
     pass
