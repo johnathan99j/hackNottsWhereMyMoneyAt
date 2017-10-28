6a4b631962e2be038444fc276ce4875327d6d3dd
Johnathan Graydon <johnathan99j@yahoo.com>
2017-10-28 20:01:20 +0100

    Make output more consistent
    The API tends to alternate lng and lat.

--- geo.py
+++ geo.py
@@ -9,7 +9,7 @@ i = 0
 
 try:
     while r.json()['data'][i]['geocode'] != "":
-        print(r.json()['data'][i]['geocode'])
+        print("lat:" + str(r.json()['data'][i]['geocode']['lat']) + " lng:" + str(r.json()['data'][i]['geocode']['lng']))
         i += 1
 except IndexError as e:
     pass
