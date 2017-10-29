12b17be92a4ff4ba2ccb56f8db588cba42b3dae0
Johnathan Graydon <johnathan99j@yahoo.com>
2017-10-29 01:35:14 +0100

    geo.py now outputs directly to map input

--- geo.py
+++ geo.py
@@ -8,6 +8,10 @@ i, j = 0, 0
 # cust id -> account(s) id -> purchase(s) id -> merchant id -> gps
 custId = "59f45071a73e4942cdafe49f"
 
+file = open("capital_one_open_bank/static/GeoJSONP.js","w")
+
+file.write('eqfeed_callback({"type": "FeatureCollection","features": [')
+
 r = requests.get("http://api.reimaginebanking.com/customers/" + custId + "/accounts?key=" + API_KEY)
 
 try:
@@ -19,8 +23,15 @@ try:
         while s.json()[j]['merchant_id'] != "":
             t = requests.get("http://api.reimaginebanking.com/merchants/" + s.json()[j]['merchant_id'] + "?key=" + API_KEY)
 
-            print("lat:" + str(t.json()['geocode']['lat']) + " lng:" + str(t.json()['geocode']['lng']))
+            # print("lat:" + str(t.json()['geocode']['lat']) + " lng:" + str(t.json()['geocode']['lng']))
+
+            file.write('{"type": "Feature","properties": {"mag": 5.1}, "geometry": {"type": "Point","coordinates": [' + str(t.json()["geocode"]["lng"]) + ', ' + str(t.json()["geocode"]["lat"]) + ']}},\n')
+
             j += 1
         i += 1
 except IndexError as e:
     pass
+
+file.write(']});')
+
+file.close()
