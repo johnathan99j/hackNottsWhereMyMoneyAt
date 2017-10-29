972418bdcbca3b0ea1e610993cb9c96950795061
dragos-home-pc <dragos-marian.chivulescu@student.manchester.ac.uk>
2017-10-29 10:27:31 +0000

    Make front-end even nicer

---
+++ capital_one_open_bank/static/colour-circles.js
@@ -0,0 +1,16 @@
+console.log("before function");
+window.onload = function(e) {
+    // var circleImageContainerArray = document.getElementById("gmnoprint");
+    // for (var i = 0; i < circleImageContainerArray.length; i++)
+    // {
+    //   var circleImage = circleImageContainer.getElementsByTagName("img");
+    //   circleImage.style.background = "green";
+    // }
+
+// Now create and append to iDiv
+
+  console.log("dada");
+  var innerDiv = document.createElement('div');
+  innerDiv.className = 'pretty-obvious-name';
+  document.getElementsByTagName('body')[0].appendChild(iDiv);
+}
