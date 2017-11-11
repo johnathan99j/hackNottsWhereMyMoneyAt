ee1c380509531a221e8fa88d10bb30d829f4eb5f
Johnathan Graydon <johnathan99j@yahoo.com>
2017-11-11 17:15:58 +0000

    Tidy up geo.py

--- geo.py
+++ geo.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python
 import requests
-import json
 import sys
-import scipy
-import scipy.interpolate
 from scipy.interpolate import interp1d
 
-from envVariables import *
+from envVariables import API_KEY
+
 
 def main():
     i, j = 0, 0
@@ -20,7 +18,7 @@ def main():
 
     custId = sys.argv[1]
 
-    file = open("capital_one_open_bank/static/GeoJSONP.js","w")
+    file = open("capital_one_open_bank/static/GeoJSONP.js", "w")
 
     file.write('eqfeed_callback({"type": "FeatureCollection","features": [')
 
@@ -34,30 +32,30 @@ def main():
             # Loop though all the purchases in this account
             while s.json()[j]['merchant_id'] != "":
                 t = requests.get("http://api.reimaginebanking.com/merchants/" + str(s.json()[j]['merchant_id']) + "?key=" + API_KEY)
-
-                # print("lat:" + str(t.json()['geocode']['lat']) + " lng:" + str(t.json()['geocode']['lng']))
-
                 file.write('{"type": "Feature","properties": {"mag": ' + str(calcMag(s.json()[j]["amount"])) + '}, "geometry": {"type": "Point","coordinates": [' + str(t.json()["geocode"]["lng"]) + ', ' + str(t.json()["geocode"]["lat"]) + ']}},\n')
 
                 j += 1
             i += 1
-    except IndexError as e:
+    except IndexError:
         pass
 
     file.write(']});')
 
     file.close()
 
+
 def calcMag(money):
 
-    if money > 512: money = 511
-    elif money < 0: money = 0
+    if money > 512:
+        money = 511
+    elif money < 0:
+        money = 0
 
-    m = interp1d([0,512],[3,6])
-    # print(m(money))
+    m = interp1d([0, 512], [3, 6])
 
     return m(money)
     pass
+
 
 if __name__ == "__main__":
     main()
